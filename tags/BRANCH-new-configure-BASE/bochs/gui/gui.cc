529d9beee66a2854203b302b3ebbcbe5a2b436f5
(no author) <(no author)@6c8e4198-a580-4521-a02f-c9ae86df1db9>
2002-03-07 02:23:55 +0000

    This commit was manufactured by cvs2svn to create tag 'BRANCH-new-configure- BASE'.
    git-svn-id: http://svn.code.sf.net/p/bochs/code@1910 6c8e4198-a580-4521-a02f-c9ae86df1db9

---
+++ tags/BRANCH-new-configure-BASE/bochs/gui/gui.cc
@@ -0,0 +1,323 @@
+/////////////////////////////////////////////////////////////////////////
+// $Id: gui.cc,v 1.28 2002-03-06 18:17:24 bdenney Exp $
+/////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (C) 2002  MandrakeSoft S.A.
+//
+//    MandrakeSoft S.A.
+//    43, rue d'Aboukir
+//    75002 Paris - France
+//    http://www.linux-mandrake.com/
+//    http://www.mandrakesoft.com/
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+
+
+#include <signal.h>
+#include "bochs.h"
+#include "gui/bitmaps/floppya.h"
+#include "gui/bitmaps/floppyb.h"
+#include "gui/bitmaps/mouse.h"
+#include "gui/bitmaps/reset.h"
+#include "gui/bitmaps/power.h"
+#include "gui/bitmaps/snapshot.h"
+#include "gui/bitmaps/configbutton.h"
+#include "gui/bitmaps/cdromd.h"
+#if BX_WITH_MACOS
+#  include <Disks.h>
+#endif
+
+
+
+
+bx_gui_c   bx_gui;
+
+#define BX_GUI_THIS bx_gui.
+#define LOG_THIS BX_GUI_THIS
+
+bx_gui_c::bx_gui_c(void)
+{
+  put("GUI"); // Init in specific_init
+  settype(GUILOG);
+}
+
+  void
+bx_gui_c::init(int argc, char **argv, unsigned tilewidth, unsigned tileheight)
+{
+  specific_init(&bx_gui, argc, argv, tilewidth, tileheight, BX_HEADER_BAR_Y);
+
+  // Define some bitmaps to use in the headerbar
+  BX_GUI_THIS floppyA_bmap_id = create_bitmap(bx_floppya_bmap,
+                          BX_FLOPPYA_BMAP_X, BX_FLOPPYA_BMAP_Y);
+  BX_GUI_THIS floppyA_eject_bmap_id = create_bitmap(bx_floppya_eject_bmap,
+                          BX_FLOPPYA_BMAP_X, BX_FLOPPYA_BMAP_Y);
+  BX_GUI_THIS floppyB_bmap_id = create_bitmap(bx_floppyb_bmap,
+                          BX_FLOPPYB_BMAP_X, BX_FLOPPYB_BMAP_Y);
+  BX_GUI_THIS floppyB_eject_bmap_id = create_bitmap(bx_floppyb_eject_bmap,
+                          BX_FLOPPYB_BMAP_X, BX_FLOPPYB_BMAP_Y);
+  BX_GUI_THIS cdromD_bmap_id = create_bitmap(bx_cdromd_bmap,
+                          BX_CDROMD_BMAP_X, BX_CDROMD_BMAP_Y);
+  BX_GUI_THIS cdromD_eject_bmap_id = create_bitmap(bx_cdromd_eject_bmap,
+                          BX_CDROMD_BMAP_X, BX_CDROMD_BMAP_Y);
+  BX_GUI_THIS mouse_bmap_id = create_bitmap(bx_mouse_bmap,
+                          BX_MOUSE_BMAP_X, BX_MOUSE_BMAP_Y);
+  BX_GUI_THIS nomouse_bmap_id = create_bitmap(bx_nomouse_bmap,
+                          BX_MOUSE_BMAP_X, BX_MOUSE_BMAP_Y);
+
+
+  BX_GUI_THIS power_bmap_id = create_bitmap(bx_power_bmap, BX_POWER_BMAP_X, BX_POWER_BMAP_Y);
+  BX_GUI_THIS reset_bmap_id = create_bitmap(bx_reset_bmap, BX_RESET_BMAP_X, BX_RESET_BMAP_Y);
+  BX_GUI_THIS snapshot_bmap_id = create_bitmap(bx_snapshot_bmap, BX_SNAPSHOT_BMAP_X, BX_SNAPSHOT_BMAP_Y);
+  BX_GUI_THIS config_bmap_id = create_bitmap(bx_config_bmap, BX_SNAPSHOT_BMAP_X, BX_SNAPSHOT_BMAP_Y);
+
+
+  // Add the initial bitmaps to the headerbar, and enable callback routine, for use
+  // when that bitmap is clicked on
+
+  // Floppy A:
+  BX_GUI_THIS floppyA_status = bx_devices.floppy->get_media_status(0);
+  if (BX_GUI_THIS floppyA_status)
+    BX_GUI_THIS floppyA_hbar_id = headerbar_bitmap(BX_GUI_THIS floppyA_bmap_id,
+                          BX_GRAVITY_LEFT, floppyA_handler);
+  else
+    BX_GUI_THIS floppyA_hbar_id = headerbar_bitmap(BX_GUI_THIS floppyA_eject_bmap_id,
+                          BX_GRAVITY_LEFT, floppyA_handler);
+
+  // Floppy B:
+  BX_GUI_THIS floppyB_status = bx_devices.floppy->get_media_status(1);
+  if (BX_GUI_THIS floppyB_status)
+    BX_GUI_THIS floppyB_hbar_id = headerbar_bitmap(BX_GUI_THIS floppyB_bmap_id,
+                          BX_GRAVITY_LEFT, floppyB_handler);
+  else
+    BX_GUI_THIS floppyB_hbar_id = headerbar_bitmap(BX_GUI_THIS floppyB_eject_bmap_id,
+                          BX_GRAVITY_LEFT, floppyB_handler);
+
+  // CDROM
+  BX_GUI_THIS cdromD_status =
+    bx_devices.hard_drive->get_cd_media_status()
+    && bx_options.cdromd.Opresent->get ();
+  if (BX_GUI_THIS cdromD_status)
+    BX_GUI_THIS cdromD_hbar_id = headerbar_bitmap(BX_GUI_THIS cdromD_bmap_id,
+                          BX_GRAVITY_LEFT, cdromD_handler);
+  else
+    BX_GUI_THIS cdromD_hbar_id = headerbar_bitmap(BX_GUI_THIS cdromD_eject_bmap_id,
+                          BX_GRAVITY_LEFT, cdromD_handler);
+
+  // Mouse button
+  if (bx_options.Omouse_enabled->get ())
+    BX_GUI_THIS mouse_hbar_id = headerbar_bitmap(BX_GUI_THIS mouse_bmap_id,
+                          BX_GRAVITY_LEFT, toggle_mouse_enable);
+  else
+    BX_GUI_THIS mouse_hbar_id = headerbar_bitmap(BX_GUI_THIS nomouse_bmap_id,
+                          BX_GRAVITY_LEFT, toggle_mouse_enable);
+
+  // Power button
+  BX_GUI_THIS power_hbar_id = headerbar_bitmap(BX_GUI_THIS power_bmap_id,
+                          BX_GRAVITY_RIGHT, power_handler);
+  // Reset button
+  BX_GUI_THIS reset_hbar_id = headerbar_bitmap(BX_GUI_THIS reset_bmap_id,
+                          BX_GRAVITY_RIGHT, reset_handler);
+  // Snapshot button
+  BX_GUI_THIS snapshot_hbar_id = headerbar_bitmap(BX_GUI_THIS snapshot_bmap_id,
+                          BX_GRAVITY_RIGHT, snapshot_handler);
+  // Configure button
+  BX_GUI_THIS config_hbar_id = headerbar_bitmap(BX_GUI_THIS config_bmap_id,
+                          BX_GRAVITY_RIGHT, config_handler);
+
+  show_headerbar();
+}
+
+void
+bx_gui_c::update_floppy_status_buttons (void) {
+  BX_GUI_THIS floppyA_status = 
+    bx_devices.floppy->get_media_status (0)
+    && bx_options.floppya.Oinitial_status->get ();
+  BX_GUI_THIS floppyB_status = 
+      bx_devices.floppy->get_media_status (1)
+      && bx_options.floppyb.Oinitial_status->get ();
+  BX_GUI_THIS cdromD_status =
+    bx_devices.hard_drive->get_cd_media_status()
+    && bx_options.cdromd.Opresent->get ();
+  if (BX_GUI_THIS floppyA_status)
+    replace_bitmap(BX_GUI_THIS floppyA_hbar_id, BX_GUI_THIS floppyA_bmap_id);
+  else {
+#if BX_WITH_MACOS
+    // If we are using the Mac floppy driver, eject the disk
+    // from the floppy drive.  This doesn't work in MacOS X.
+    if (!strcmp(bx_options.floppya.Opath->get (), SuperDrive))
+      DiskEject(1);
+#endif
+    replace_bitmap(BX_GUI_THIS floppyA_hbar_id, BX_GUI_THIS floppyA_eject_bmap_id);
+    }
+  if (BX_GUI_THIS floppyB_status)
+    replace_bitmap(BX_GUI_THIS floppyB_hbar_id, BX_GUI_THIS floppyB_bmap_id);
+  else {
+#if BX_WITH_MACOS
+    // If we are using the Mac floppy driver, eject the disk
+    // from the floppy drive.  This doesn't work in MacOS X.
+    if (!strcmp(bx_options.floppyb.Opath->get (), SuperDrive))
+      DiskEject(1);
+#endif
+    replace_bitmap(BX_GUI_THIS floppyB_hbar_id, BX_GUI_THIS floppyB_eject_bmap_id);
+    }
+  if (BX_GUI_THIS cdromD_status)
+    replace_bitmap(BX_GUI_THIS cdromD_hbar_id, BX_GUI_THIS cdromD_bmap_id);
+  else {
+    replace_bitmap(BX_GUI_THIS cdromD_hbar_id, BX_GUI_THIS cdromD_eject_bmap_id);
+    }
+}
+
+  void
+bx_gui_c::floppyA_handler(void)
+{
+  BX_GUI_THIS floppyA_status = !BX_GUI_THIS floppyA_status;
+  bx_devices.floppy->set_media_status(0, BX_GUI_THIS floppyA_status);
+  BX_GUI_THIS update_floppy_status_buttons ();
+}
+
+  void
+bx_gui_c::floppyB_handler(void)
+{
+  BX_GUI_THIS floppyB_status = !BX_GUI_THIS floppyB_status;
+  bx_devices.floppy->set_media_status(1, BX_GUI_THIS floppyB_status);
+  BX_GUI_THIS update_floppy_status_buttons ();
+}
+
+  void
+bx_gui_c::cdromD_handler(void)
+{
+  BX_GUI_THIS cdromD_status =
+    bx_devices.hard_drive->set_cd_media_status(!BX_GUI_THIS cdromD_status);
+  BX_GUI_THIS update_floppy_status_buttons ();
+}
+
+  void
+bx_gui_c::reset_handler(void)
+{
+  BX_INFO(( "system RESET callback." ));
+  bx_pc_system.ResetSignal( PCS_SET ); /* XXX is this right? */
+  for (int i=0; i<BX_SMP_PROCESSORS; i++)
+      BX_CPU(i)->reset(BX_RESET_HARDWARE);
+}
+
+  void
+bx_gui_c::power_handler(void)
+{
+  // the user pressed power button, so there's no doubt they want bochs
+  // to quit.  Change panics to fatal for the GUI and then do a panic.
+  LOG_THIS setonoff(LOGLEV_PANIC, ACT_FATAL);
+  BX_PANIC (("POWER button turned off."));
+  // shouldn't reach this point, but if you do, QUIT!!!
+  fprintf (stderr, "Bochs is exiting because you pressed the power button.\n");
+  ::exit (1);
+}
+
+  void
+bx_gui_c::snapshot_handler(void)
+{
+  Bit8u* text_snapshot = NULL;
+  char *snapshot_txt;
+  unsigned line_addr, txt_addr, txHeight, txWidth;
+#if defined(WIN32) && !defined(__MINGW32__)
+  HANDLE hMem;
+#else
+  FILE *OUTPUT;
+#endif
+
+  bx_vga.get_text_snapshot(&text_snapshot, &txHeight, &txWidth);
+  if (txHeight > 0) {
+    snapshot_txt = (char*) malloc(txHeight*(txWidth+2)+1);
+    txt_addr = 0;
+    for (unsigned i=0; i<txHeight; i++) {
+      line_addr = i * txWidth * 2;
+      for (unsigned j=0; j<(txWidth*2); j+=2) {
+        snapshot_txt[txt_addr] = text_snapshot[line_addr+j];
+        txt_addr++;
+      }
+#if defined(WIN32) && !defined(__MINGW32__)
+      snapshot_txt[txt_addr] = 13;
+      txt_addr++;
+#endif
+      snapshot_txt[txt_addr] = 10;
+      txt_addr++;
+    }
+    snapshot_txt[txt_addr] = 0;
+#if defined(WIN32) && !defined(__MINGW32__)
+    if (OpenClipboard(NULL)) {
+      hMem = GlobalAlloc(GMEM_ZEROINIT, txHeight*(txWidth+2)+1);
+      EmptyClipboard();
+      lstrcpy((char *)hMem, snapshot_txt);
+      SetClipboardData(CF_TEXT, hMem);
+      CloseClipboard();
+      GlobalFree(hMem);
+    }
+#else
+    OUTPUT = fopen("snapshot.txt", "w");
+    fwrite(snapshot_txt, 1, strlen(snapshot_txt), OUTPUT);
+    fclose(OUTPUT);
+#endif
+    free(snapshot_txt);
+  } else {
+    BX_INFO(( "# SNAPSHOT callback (graphics mode unimplemented)." ));
+  }
+}
+
+  void
+bx_gui_c::config_handler(void)
+{
+#if BX_USE_CONTROL_PANEL
+  bx_control_panel (BX_CPANEL_RUNTIME);
+#else
+  BX_INFO(( "# CONFIG callback (unimplemented)." ));
+#endif
+}
+
+  void
+bx_gui_c::toggle_mouse_enable(void)
+{
+  int old = bx_options.Omouse_enabled->get ();
+  BX_DEBUG (("toggle mouse_enabled, now %d", !old));
+  bx_options.Omouse_enabled->set (!old);
+}
+
+  void
+bx_gui_c::mouse_enabled_changed (Boolean val)
+{
+  // This is only called when SIM->get_init_done is 1.  Note that VAL
+  // is the new value of mouse_enabled, which may not match the old
+  // value which is still in bx_options.Omouse_enabled->get ().
+  BX_DEBUG (("replacing the mouse bitmaps"));
+  if (val)
+    replace_bitmap(BX_GUI_THIS mouse_hbar_id, BX_GUI_THIS mouse_bmap_id);
+  else
+    replace_bitmap(BX_GUI_THIS mouse_hbar_id, BX_GUI_THIS nomouse_bmap_id);
+  // give the GUI a chance to respond to the event.  Most guis will hide
+  // the native mouse cursor and do something to trap the mouse inside the
+  // bochs VGA display window.
+  mouse_enabled_changed_specific (val);
+}
+
+void 
+bx_gui_c::init_signal_handlers ()
+{
+#if BX_GUI_SIGHANDLER
+  Bit32u mask = get_sighandler_mask ();
+  for (Bit32u sig=0; sig<32; sig++)
+  {
+    if (mask & (1<<sig))
+      signal (sig, bx_signal_handler);
+  }
+#endif
+}
---
+++ tags/BRANCH-new-configure-BASE/bochs/gui/keymap.cc
@@ -0,0 +1,226 @@
+/////////////////////////////////////////////////////////////////////////
+// $Id: keymap.cc,v 1.3 2002-03-06 14:04:42 cbothamy Exp $
+/////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (C) 2001  Christophe Bothamy
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+
+/////////////////////////////////////////////////////////////////////////
+//
+// Todo
+//  . Currently supported only by x11. Check if other guis need mapping.
+//  . Tables look-up should be optimised.
+//
+
+#include "bochs.h"
+
+// Table of bocks "BX_KEY_*" symbols
+// the table must be in BX_KEY_* order
+char *bx_key_symbol[BX_KEY_NBKEYS] = {
+  "BX_KEY_CTRL_L",         "BX_KEY_SHIFT_L",        "BX_KEY_F1",
+  "BX_KEY_F2",             "BX_KEY_F3",             "BX_KEY_F4",
+  "BX_KEY_F5",             "BX_KEY_F6",             "BX_KEY_F7",
+  "BX_KEY_F8",             "BX_KEY_F9",             "BX_KEY_F10",
+  "BX_KEY_F11",            "BX_KEY_F12",            "BX_KEY_CTRL_R",
+  "BX_KEY_SHIFT_R",        "BX_KEY_CAPS_LOCK",      "BX_KEY_NUM_LOCK",
+  "BX_KEY_ALT_L",          "BX_KEY_ALT_R",          "BX_KEY_A",
+  "BX_KEY_B",              "BX_KEY_C",              "BX_KEY_D",
+  "BX_KEY_E",              "BX_KEY_F",              "BX_KEY_G",
+  "BX_KEY_H",              "BX_KEY_I",              "BX_KEY_J",
+  "BX_KEY_K",              "BX_KEY_L",              "BX_KEY_M",
+  "BX_KEY_N",              "BX_KEY_O",              "BX_KEY_P",
+  "BX_KEY_Q",              "BX_KEY_R",              "BX_KEY_S",
+  "BX_KEY_T",              "BX_KEY_U",              "BX_KEY_V",
+  "BX_KEY_W",              "BX_KEY_X",              "BX_KEY_Y",
+  "BX_KEY_Z",              "BX_KEY_0",              "BX_KEY_1",
+  "BX_KEY_2",              "BX_KEY_3",              "BX_KEY_4",
+  "BX_KEY_5",              "BX_KEY_6",              "BX_KEY_7",
+  "BX_KEY_8",              "BX_KEY_9",              "BX_KEY_ESC",
+  "BX_KEY_SPACE",          "BX_KEY_SINGLE_QUOTE",   "BX_KEY_COMMA",
+  "BX_KEY_PERIOD",         "BX_KEY_SLASH",          "BX_KEY_SEMICOLON",
+  "BX_KEY_EQUALS",         "BX_KEY_LEFT_BRACKET",   "BX_KEY_BACKSLASH",
+  "BX_KEY_RIGHT_BRACKET",  "BX_KEY_MINUS",          "BX_KEY_GRAVE",
+  "BX_KEY_BACKSPACE",      "BX_KEY_ENTER",          "BX_KEY_TAB",
+  "BX_KEY_LEFT_BACKSLASH", "BX_KEY_PRINT",          "BX_KEY_SCRL_LOCK",
+  "BX_KEY_PAUSE",          "BX_KEY_INSERT",         "BX_KEY_DELETE",
+  "BX_KEY_HOME",           "BX_KEY_END",            "BX_KEY_PAGE_UP",
+  "BX_KEY_PAGE_DOWN",      "BX_KEY_KP_ADD",         "BX_KEY_KP_SUBTRACT",
+  "BX_KEY_KP_END",         "BX_KEY_KP_DOWN",        "BX_KEY_KP_PAGE_DOWN",
+  "BX_KEY_KP_LEFT",        "BX_KEY_KP_RIGHT",       "BX_KEY_KP_HOME",
+  "BX_KEY_KP_UP",          "BX_KEY_KP_PAGE_UP",     "BX_KEY_KP_INSERT",
+  "BX_KEY_KP_DELETE",      "BX_KEY_KP_5",           "BX_KEY_UP",
+  "BX_KEY_DOWN",           "BX_KEY_LEFT",           "BX_KEY_RIGHT",
+  "BX_KEY_KP_ENTER",       "BX_KEY_KP_MULTIPLY",    "BX_KEY_KP_DIVIDE",
+  "BX_KEY_WIN_L",          "BX_KEY_WIN_R",          "BX_KEY_MENU",           
+  "BX_KEY_ALT_SYSREQ",     "BX_KEY_CTRL_BREAK",     "BX_KEY_INT_BACK",       
+  "BX_KEY_INT_FORWARD",    "BX_KEY_INT_STOP",       "BX_KEY_INT_MAIL",       
+  "BX_KEY_INT_SEARCH",     "BX_KEY_INT_FAV",        "BX_KEY_INT_HOME",       
+  "BX_KEY_POWER_MYCOMP",   "BX_KEY_POWER_CALC",     "BX_KEY_POWER_SLEEP",    
+  "BX_KEY_POWER_POWER",    "BX_KEY_POWER_WAKE",
+  };
+
+bx_keymap_c bx_keymap;
+
+#define LOG_THIS bx_keymap.
+
+bx_keymap_c::bx_keymap_c(void)
+{
+    put("KMAP");
+
+    keymapCount = 0;
+    keymapTable = (KeyEntry *)NULL;
+
+}
+
+bx_keymap_c::~bx_keymap_c(void)
+{
+    if(keymapTable != NULL) {
+      free(keymapTable);
+      keymapTable = (KeyEntry *)NULL;
+      }
+    keymapCount = 0;
+}
+
+    void
+bx_keymap_c::loadKeymap(Bit32u stringToSymbol(const char*))
+{
+  if(bx_options.keyboard.OuseMapping->get()) {
+    loadKeymap(stringToSymbol,bx_options.keyboard.Okeymap->getptr());
+    }
+}
+
+    void
+bx_keymap_c::loadKeymap(Bit32u stringToSymbol(const char*), const char* filename)
+{
+    FILE   *keymapFile;
+    char   line[256], c, *p, *bxSymbol, *guiSymbol;
+    Bit32u bxKey, guiKey;
+    Bit16u lineCount=0;
+
+    if((keymapFile = fopen(filename,"r"))==NULL) {
+      BX_PANIC(("Can not open keymap file '%s'.",filename));
+      }
+    
+    BX_INFO(("Loading keymap from '%s'",filename));
+
+    // Read keymap file one line at a time
+    while(1) {
+      fgets(line, sizeof(line)-1, keymapFile);
+      lineCount++;
+
+      // End of file
+      if(feof(keymapFile)) break;
+
+      // silently drop till end of line
+      if( (p=strchr(line,'\n')) == NULL) {
+        do {
+          c = fgetc(keymapFile);
+          } while((c!='\n')&&(c!=0));
+        }
+      else {
+        *p = 0;
+        }
+
+      // drop comments
+      p = strchr(line,'#');
+      if (p!=NULL) *p = 0;
+
+      // If string is empty
+      p = line; while (*p!=0 && *p==' ') p++;
+      if (*p==0) continue;
+
+      // Parse line, find '=', split line
+      p = strchr(line,'=');
+      if (p==NULL) {
+        BX_INFO(("line %d: Unknown format",lineCount));
+        continue;
+        }
+      else {
+        *p = 0;
+        guiSymbol = p+1;
+        }
+
+      // Find first and last char of gui key symbol
+      while (*guiSymbol==' ') guiSymbol++;
+      p=guiSymbol; while (*p!=0 && *p!=' ') p++; *p=0;
+
+      // Find first and last char of BX_KEY
+      bxSymbol=line; while (*bxSymbol==' ') bxSymbol++;
+      p=bxSymbol; while (*p!=0 && *p!=' ') p++; *p=0;
+
+      // Here we have the two symbols
+      bxKey = convertStringToBXKey(bxSymbol);
+      guiKey = stringToSymbol(guiSymbol);
+      
+      // Check if data is valid
+      if( bxKey==BX_KEYMAP_UNKNOWN ) {
+        BX_INFO(("line %d: Ignoring unknown BX_KEY constant '%s'",lineCount,bxSymbol));
+        continue;
+        }
+
+      if( guiKey==BX_KEYMAP_UNKNOWN ) {
+        BX_INFO(("line %d: Ignoring unknown GUI constant '%s'",lineCount,guiSymbol));
+        continue;
+        }
+
+      keymapTable=(KeyEntry*)realloc(keymapTable,(keymapCount+1) * sizeof(KeyEntry));
+      
+      if(keymapTable==NULL) 
+        BX_PANIC(("Can not allocate memory for keymap table."));
+
+      keymapTable[keymapCount].bxKey=bxKey;
+      keymapTable[keymapCount].guiKey=guiKey;
+      
+      keymapCount++;
+      }
+
+    BX_INFO(("Loaded %d symbols",keymapCount));
+
+    fclose(keymapFile);
+}
+
+    Bit32u
+bx_keymap_c::convertStringToBXKey(const char* string)
+{
+    Bit16u i;
+
+    // We look through the bx_key_symbol table to find the searched string
+    for (i=0; i<BX_KEY_NBKEYS; i++) {
+      if (strcmp(string,bx_key_symbol[i])==0) {
+        return i;
+        }
+      }
+  
+    // Key is not known
+    return BX_KEYMAP_UNKNOWN;
+}
+
+    Bit32u
+bx_keymap_c::getBXKey(Bit32u key)
+{
+    Bit16u i;
+
+    // We look through the keymap table to find the searched key
+    for (i=0; i<keymapCount; i++) {
+      if (keymapTable[i].guiKey == key) {
+        return keymapTable[i].bxKey;
+        }
+      }
+
+    // Return default
+    return BX_KEY_UNHANDLED;
+}
+
