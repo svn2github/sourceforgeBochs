428df21b4d5ac866bd6df4a242af30c1d2bea457
(no author) <(no author)@6c8e4198-a580-4521-a02f-c9ae86df1db9>
2004-01-11 21:20:01 +0000

    This commit was manufactured by cvs2svn to create tag 'REL_2_1_FINAL'.
    git-svn-id: http://svn.code.sf.net/p/bochs/code@5441 6c8e4198-a580-4521-a02f-c9ae86df1db9

---
+++ tags/REL_2_1_FINAL/bochs/gui/win32dialog.cc
@@ -0,0 +1,261 @@
+/////////////////////////////////////////////////////////////////////////
+// $Id: win32dialog.cc,v 1.10 2003-10-24 15:39:57 vruppert Exp $
+/////////////////////////////////////////////////////////////////////////
+
+#ifdef WIN32
+
+#include "config.h"
+
+extern "C" {
+#include <stdio.h>
+#include <windows.h>
+}
+#include "win32res.h"
+#include "siminterface.h"
+#include "win32dialog.h"
+
+HWND GetBochsWindow()
+{
+  HWND hwnd;
+
+  hwnd = FindWindow("Bochs for Windows", NULL);
+  if (hwnd == NULL) {
+    hwnd = GetForegroundWindow();
+  }
+  return hwnd;
+}
+
+BOOL CreateImage(HWND hDlg, int sectors, const char *filename)
+{
+  if (sectors < 1) {
+    MessageBox(hDlg, "The disk size is invalid.", "Invalid size", MB_ICONERROR);
+    return FALSE;
+  }
+  if (lstrlen(filename) < 1) {
+    MessageBox(hDlg, "You must type a file name for the new disk image.", "Bad filename", MB_ICONERROR);
+    return FALSE;
+  }
+  int ret = SIM->create_disk_image (filename, sectors, 0);
+  if (ret == -1) {  // already exists
+    int answer = MessageBox(hDlg, "File exists.  Do you want to overwrite it?",
+                            "File exists", MB_YESNO);
+    if (answer == IDYES)
+      ret = SIM->create_disk_image (filename, sectors, 1);
+    else 
+      return FALSE;
+  }
+  if (ret == -2) {
+    MessageBox(hDlg, "I could not create the disk image. Check for permission problems or available disk space.", "Failed", MB_ICONERROR);
+    return FALSE;
+  }
+  return TRUE;
+}
+
+static BOOL CALLBACK LogAskProc(HWND hDlg, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+  BxEvent *event;
+  int level;
+
+  switch (msg) {
+    case WM_INITDIALOG:
+      event = (BxEvent*)lParam;
+      level = event->u.logmsg.level;
+      SetWindowText(hDlg, SIM->get_log_level_name(level));
+      SetWindowText(GetDlgItem(hDlg, IDASKDEV), event->u.logmsg.prefix);
+      SetWindowText(GetDlgItem(hDlg, IDASKMSG), event->u.logmsg.msg);
+      SendMessage(GetDlgItem(hDlg, IDASKLIST), LB_ADDSTRING, 0, (LPARAM)"Continue");
+      SendMessage(GetDlgItem(hDlg, IDASKLIST), LB_ADDSTRING, 0, (LPARAM)"Continue and don't ask again");
+      SendMessage(GetDlgItem(hDlg, IDASKLIST), LB_ADDSTRING, 0, (LPARAM)"Kill simulation");
+      SendMessage(GetDlgItem(hDlg, IDASKLIST), LB_ADDSTRING, 0, (LPARAM)"Abort (dump core)");
+#if BX_DEBUGGER
+      SendMessage(GetDlgItem(hDlg, IDASKLIST), LB_ADDSTRING, 0, (LPARAM)"Continue and return to debugger");
+#endif
+      SendMessage(GetDlgItem(hDlg, IDASKLIST), LB_SETCURSEL, 2, 0);
+      SetFocus(GetDlgItem(hDlg, IDASKLIST));
+      return FALSE;
+    case WM_CLOSE:
+      EndDialog(hDlg, BX_LOG_ASK_CHOICE_DIE);
+      break;
+    case WM_COMMAND:
+      switch (LOWORD(wParam)) {
+        case IDOK:
+          EndDialog(hDlg, SendMessage(GetDlgItem(hDlg, IDASKLIST), LB_GETCURSEL, 0, 0));
+          break;
+        case IDCANCEL:
+          EndDialog(hDlg, BX_LOG_ASK_CHOICE_DIE);
+          break;
+    }
+  }
+  return FALSE;
+}
+
+static BOOL CALLBACK StringParamProc(HWND hDlg, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+  static bx_param_string_c *param;
+  char buffer[20];
+
+  switch (msg) {
+    case WM_INITDIALOG:
+      param = (bx_param_string_c *)lParam;
+      SetWindowText(hDlg, param->get_name());
+      SetWindowText(GetDlgItem(hDlg, IDSTRING), param->getptr());
+      return FALSE;
+    case WM_CLOSE:
+      EndDialog(hDlg, -1);
+      break;
+    case WM_COMMAND:
+      switch (LOWORD(wParam)) {
+        case IDOK:
+          GetWindowText(GetDlgItem(hDlg, IDSTRING), buffer, 20);
+          param->set(buffer);
+          EndDialog(hDlg, 1);
+          break;
+        case IDCANCEL:
+          EndDialog(hDlg, -1);
+          break;
+    }
+  }
+  return FALSE;
+}
+
+static BOOL CALLBACK FloppyDlgProc(HWND hDlg, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+  static bx_param_filename_c *param;
+  static bx_param_enum_c *status;
+  static bx_param_enum_c *disktype;
+  static char origpath[MAX_PATH];
+  char mesg[MAX_PATH];
+  char path[MAX_PATH];
+  char *title;
+  int cap;
+
+  switch (msg) {
+    case WM_INITDIALOG:
+      param = (bx_param_filename_c *)lParam;
+      if (param->get_id() == BXP_FLOPPYA_PATH) {
+        status = SIM->get_param_enum(BXP_FLOPPYA_STATUS);
+        disktype = SIM->get_param_enum(BXP_FLOPPYA_DEVTYPE);
+      } else {
+        status = SIM->get_param_enum(BXP_FLOPPYB_STATUS);
+        disktype = SIM->get_param_enum(BXP_FLOPPYB_DEVTYPE);
+      }
+      cap = disktype->get() - disktype->get_min();
+      SetWindowText(GetDlgItem(hDlg, IDDEVTYPE), floppy_type_names[cap]);
+      if (status->get() == BX_INSERTED) {
+        SendMessage(GetDlgItem(hDlg, IDSTATUS), BM_SETCHECK, BST_CHECKED, 0);
+      }
+      lstrcpy(origpath, param->getptr());
+      title = param->get_label();
+      if (!title) title = param->get_name();
+      SetWindowText(hDlg, title);
+      if (lstrlen(origpath) && lstrcmp(origpath, "none")) {
+        SetWindowText(GetDlgItem(hDlg, IDPATH), origpath);
+      }
+      return FALSE;
+    case WM_CLOSE:
+      EndDialog(hDlg, -1);
+      break;
+    case WM_COMMAND:
+      switch (LOWORD(wParam)) {
+        case IDBROWSE:
+          if (AskFilename(hDlg, param) > 0) {
+            SetWindowText(GetDlgItem(hDlg, IDPATH), param->getptr());
+            SendMessage(GetDlgItem(hDlg, IDSTATUS), BM_SETCHECK, BST_CHECKED, 0);
+          }
+          break;
+        case IDOK:
+          if (SendMessage(GetDlgItem(hDlg, IDSTATUS), BM_GETCHECK, 0, 0) == BST_CHECKED) {
+            GetWindowText(GetDlgItem(hDlg, IDPATH), path, MAX_PATH);
+            if (lstrlen(path)) {
+              status->set(BX_INSERTED);
+            } else {
+              status->set(BX_EJECTED);
+              lstrcpy(path, "none");
+            }
+          } else {
+            status->set(BX_EJECTED);
+            lstrcpy(path, "none");
+          }
+          param->set(path);
+          EndDialog(hDlg, 1);
+          break;
+        case IDCANCEL:
+          param->set(origpath);
+          EndDialog(hDlg, -1);
+          break;
+        case IDCREATE:
+          GetWindowText(GetDlgItem(hDlg, IDPATH), path, MAX_PATH);
+          cap = disktype->get() - disktype->get_min();
+          if (CreateImage(hDlg, floppy_type_n_sectors[cap], path)) {
+            wsprintf(mesg, "Created a %s disk image called %s", floppy_type_names[cap], path);
+            MessageBox(hDlg, mesg, "Image created", MB_OK);
+          }
+          break;
+    }
+  }
+  return FALSE;
+}
+
+void LogAskDialog(BxEvent *event)
+{
+  event->retcode = DialogBoxParam(NULL, MAKEINTRESOURCE(ASK_DLG), GetBochsWindow(),
+                                  (DLGPROC)LogAskProc, (LPARAM)event);
+}
+
+int AskFilename(HWND hwnd, bx_param_filename_c *param)
+{
+  OPENFILENAME ofn;
+  int ret;
+  char filename[MAX_PATH];
+  char *title;
+
+  param->get(filename, MAX_PATH);
+  title = param->get_label();
+  if (!title) title = param->get_name();
+  ofn.lStructSize = sizeof(OPENFILENAME);
+  ofn.hwndOwner = hwnd;
+  ofn.hInstance   = NULL;
+  ofn.lpstrCustomFilter = NULL;
+  ofn.nMaxCustFilter = 0;
+  ofn.nFilterIndex = 0;
+  ofn.lpstrFile   = filename;
+  ofn.nMaxFile    = MAX_PATH;
+  ofn.lpstrFileTitle = NULL;
+  ofn.nMaxFileTitle = 0;
+  ofn.lpstrInitialDir = NULL;
+  ofn.lpstrTitle = title;
+  ofn.nFileOffset = 0;
+  ofn.nFileExtension = 0;
+  ofn.lCustData = 0;
+  ofn.lpfnHook = NULL;
+  ofn.lpTemplateName = NULL;
+  ofn.Flags = OFN_EXPLORER | OFN_HIDEREADONLY;
+  if (param->get_options()->get() & bx_param_filename_c::SAVE_FILE_DIALOG) {
+    ofn.lpstrFilter = "Text files (*.txt)\0*.txt\0All files (*.*)\0*.*\0";
+    ofn.lpstrDefExt = "prg";
+    ofn.Flags |= OFN_OVERWRITEPROMPT;
+    ret = GetSaveFileName(&ofn);
+  } else {
+    ofn.lpstrFilter = "Floppy image files (*.img)\0*.img\0All files (*.*)\0*.*\0";
+    ofn.lpstrDefExt = "img";
+    ofn.Flags |= OFN_FILEMUSTEXIST;
+    ret = GetOpenFileName(&ofn);
+  }
+  param->set(filename);
+  if (ret == 0) ret = -1;
+  return ret;
+}
+
+int AskString(bx_param_string_c *param)
+{
+  return DialogBoxParam(NULL, MAKEINTRESOURCE(STRING_DLG), GetBochsWindow(),
+                        (DLGPROC)StringParamProc, (LPARAM)param);
+}
+
+int FloppyDialog(bx_param_filename_c *param)
+{
+  return DialogBoxParam(NULL, MAKEINTRESOURCE(FLOPPY_DLG), GetBochsWindow(),
+                        (DLGPROC)FloppyDlgProc, (LPARAM)param);
+}
+
+#endif // WIN32
