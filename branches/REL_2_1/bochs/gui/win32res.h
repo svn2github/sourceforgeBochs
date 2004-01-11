4237fb840e56948400db0890fc53706ff3f017c6
(no author) <(no author)@6c8e4198-a580-4521-a02f-c9ae86df1db9>
2004-01-11 21:08:12 +0000

    This commit was manufactured by cvs2svn to create branch 'REL_2_1'.
    git-svn-id: http://svn.code.sf.net/p/bochs/code@5438 6c8e4198-a580-4521-a02f-c9ae86df1db9

---
+++ branches/REL_2_1/bochs/gui/win32res.h
@@ -0,0 +1,18 @@
+#define ICON_BOCHS  1000
+
+#define ASK_DLG     2000
+#define IDASKTX1    2010
+#define IDASKDEV    2020
+#define IDASKTX2    2030
+#define IDASKMSG    2040
+#define IDASKLIST   2050
+#define STRING_DLG  2100
+#define IDSTRING    2110
+#define FLOPPY_DLG  2200
+#define IDDEVTX     2205
+#define IDDEVTYPE   2210
+#define IDPATHTX    2215
+#define IDPATH      2220
+#define IDBROWSE    2230
+#define IDSTATUS    2240
+#define IDCREATE    2250
