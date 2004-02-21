21ea35e7d0429eba8430d6c0b2426e8af0791ffa
(no author) <(no author)@6c8e4198-a580-4521-a02f-c9ae86df1db9>
2004-02-21 13:02:45 +0000

    This commit was manufactured by cvs2svn to create tag 'softfloat-fpu- implementation_ver2_branch_root'.
    git-svn-id: http://svn.code.sf.net/p/bochs/code@5614 6c8e4198-a580-4521-a02f-c9ae86df1db9

---
+++ tags/softfloat-fpu-implementation_ver2_branch_root/bochs/iodev/serial_raw.h
@@ -0,0 +1,30 @@
+/////////////////////////////////////////////////////////////////////////
+// $Id: serial_raw.h,v 1.4 2004-01-18 11:58:07 vruppert Exp $
+/////////////////////////////////////////////////////////////////////////
+//
+
+#if USE_RAW_SERIAL
+
+#ifdef __linux__
+#include <linux/serial.h>
+#endif
+
+#define P_EVEN 0
+#define P_ODD 1
+#define C_BREAK 201
+
+class serial_raw : public logfunctions {
+  public:
+    serial_raw (char *devname);
+    ~serial_raw (void);
+    void set_baudrate (int rate);
+    void set_data_bits (int );
+    void set_stop_bits (int);
+    void set_parity_mode (int, int);
+    void transmit (int byte);
+    void send_hangup ();
+    int ready_transmit ();
+    int ready_receive ();
+    int receive ();
+};
+#endif
